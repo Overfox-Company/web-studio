ce4c0bc6ab1c655328808cfd49b50ab420fbcc62
overfox-company <overfoxcompany@gmail.com>
2025-02-26 15:23:48 -0400

    key

--- src/app/dashboard/components/Canvas.tsx
+++ src/app/dashboard/components/Canvas.tsx
@@ -1,5 +1,5 @@
 
-import { NextPage } from 'next'
+
 import { useState } from "react";
 import * as React from 'react';
 
@@ -7,7 +7,9 @@ import { ButtonCustom, DraggableComponent } from './Components';
 
 
 const Canvas = () => {
-    const [components, setComponents] = useState<{ id: number; name: string; style: React.CSSProperties }[]>([]);
+    const [components, setComponents] = useState<{
+        id: number; name: string; style: React.CSSProperties
+    }[]>([]);
 
     const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
     // Agregar un nuevo componente al lienzo basado en el tipo de componente arrastrado
@@ -50,15 +52,8 @@ const Canvas = () => {
 
 
 
-    const [styles, setStyles] = useState<React.CSSProperties>({
-        backgroundColor: "", // color de fondo
-        color: "", // color del texto
-        border: "1px solid #ccc", // borde
-    });
+    // const [color, setColor] = useState('')
 
-
-
-    const [color, setColor] = useState('')
     const [buttons, setButtons] = useState<
         { id: number; style: React.CSSProperties }[]
     >([
@@ -112,12 +107,14 @@ const Canvas = () => {
 
                     <div>
                         {buttons.map((button, index) => (
-                            <div>
+                            <div
+                                key={button.id}
+                            >
                                 <div draggable
                                     onDragStart={(e) => e.dataTransfer.setData("component", JSON.stringify(button))}
                                 >
                                     <ButtonCustom
-                                        key={button.id}
+
                                         style={button.style}
                                     >
                                         Botón {index + 1}
@@ -157,13 +154,7 @@ const Canvas = () => {
                     )}
                 </div>
 
-                <div
-                    style={{ backgroundColor: color }}
-                    draggable
-                    onDragStart={(e) => e.dataTransfer.setData("component", "Tarjeta")}
-                >
-                    Agregar Tarjeta
-                </div>
+
             </div>
 
             {/* Lienzo donde se agregan los componentes */}
